910206e932fea5199a33bf451e9fb54d1ae0451c
Plasticc66 <xopxe9987@yandex.ru>
2022-04-28 13:49:12 +0300

    lvl3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,5 +4,7 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Brain Games!");
+        System.out.println("May i have  your name?");
+        System.out.println("Hello, " + Cli.name());
     }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,10 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static String name(){
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        return name;
+    }
+}
