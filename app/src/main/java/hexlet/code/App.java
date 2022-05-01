d55d5f6b7ddb95f53ed33b60638029e057844149
Plasticc66 <xopxe9987@yandex.ru>
2022-05-01 15:50:00 +0300

    add build shield?

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,6 @@
 package hexlet.code;
-
 public class App {
     public static void main(String[] args) {
-
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May i have  your name?");
         System.out.println("Hello, " + Cli.name());
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,7 +2,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static String name(){
+    public static String name() {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         return name;
