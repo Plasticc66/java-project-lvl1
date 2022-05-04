6005ff4379ea6969e1e367a28639fc694b870201
Plasticc66 <xopxe9987@yandex.ru>
2022-05-04 19:53:06 +0300

    add 1 choice

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,23 @@
 package hexlet.code;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May i have  your name?");
-        System.out.println("Hello, " + Cli.name());
+
+        System.out.println("Please enter the game number and press Enter\n1 - Greet\n2 - name2\n3 - name3\n4 - name4\n5 " +
+                "- name5\n0 - Exit");
+        System.out.print("your choice: ");
+        switch (enterGame.choiceGame()){
+            case 1: System.out.println("Welcome to the Brain Games!");
+                System.out.println("May i have  your name?");
+                System.out.println("Hello, " + Cli.name());
+                break;
+            case 0: System.out.println("Goodbye, have a nice day!");
+            break;
+            default:
+                System.out.println("wrong data T_T");
+        }
+
+
+
+
     }
 }
---
+++ app/src/main/java/hexlet/code/enterGame.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class enterGame {
+    public static int choiceGame() {
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = Integer.parseInt(scanner.nextLine());
+        return choice;
+    }
+}
