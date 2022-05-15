462e84043378b0c31c6783d58733a79e80114b27
Plasticc66 <xopxe9987@yandex.ru>
2022-05-15 16:55:31 +0300

    delete \n on start choise

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -17,7 +17,7 @@ public class App {
         final int primeNum = 6;
         final int exitNum = 0;
 
-        System.out.println("\nPlease enter the game number and press Enter."
+        System.out.println("Please enter the game number and press Enter."
                 + "\n1 - Greet"
                 + "\n2 - Even"
                 + "\n3 - Calc"
