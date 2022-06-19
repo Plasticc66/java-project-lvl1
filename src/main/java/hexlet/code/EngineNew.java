35e71ef4b79ad0cb672f1841c5682a61f345b639
Plasticc66 <xopxe9987@yandex.ru>
2022-06-19 15:13:43 +0300

    work progression, but NoName

--- src/main/java/hexlet/code/EngineNew.java
+++ src/main/java/hexlet/code/EngineNew.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class EngineNew {
 
     private static final int WIN_FOR_END = 3;
+
     public static int getWinForEnd() {
         return WIN_FOR_END;
     }
+
     private static int winStreakForCongrats = 0;
+
     public static int getWinStreakForCongrats() {
         return winStreakForCongrats;
     }
+
     private static int WIN_STREAK_FOR_CONGRATS = 0;
     private static boolean lose = false;
+
     public static boolean itLose() {
         return lose;
     }
@@ -24,23 +29,28 @@ public class EngineNew {
         String name = "NoName";
         System.out.println(rule);
 
-        for (int i = 0; i < questionsAndAnswers.length; i=+2){
-            System.out.println(questionsAndAnswers[i]);
-            String playerAnswer = scanner.nextLine();
 
-            if (questionsAndAnswers[i + 1].equals(playerAnswer)) {
-                System.out.println("Correct!");
-                WIN_STREAK_FOR_CONGRATS++;
-            } else {
-                System.out.println("'" + playerAnswer + "' is wrong answer ;(. Correct answer was '"
-                        + questionsAndAnswers[i+1]
-                        + "'.\nLet's try again, " + "NONAME" + "!");
-                lose = true;
+        for (int i = 0; i < questionsAndAnswers.length - 1; i += 2) {
+            if (!EngineNew.itLose()) {
+                System.out.println(questionsAndAnswers[i]);
+                String playerAnswer = scanner.nextLine();
+
+                if (questionsAndAnswers[i + 1].equals(playerAnswer)) {
+                    System.out.println("Correct!");
+                    WIN_STREAK_FOR_CONGRATS++;
+                } else {
+                    System.out.println("'" + playerAnswer + "' is wrong answer ;(. Correct answer was '"
+                            + questionsAndAnswers[i + 1]
+                            + "'.\nLet's try again, " + name + "!");
+                    lose = true;
+                    //break;
+                }
             }
 
             if (WIN_STREAK_FOR_CONGRATS == WIN_FOR_END) {
-                System.out.println("Congratulations, " + "NONAME" + "!");
+                System.out.println("Congratulations, " + name + "!");
             }
         }
+
     }
 }
--- src/main/java/hexlet/code/games/ProgressionNew.java
+++ src/main/java/hexlet/code/games/ProgressionNew.java
@@ -1,17 +1,16 @@
 package hexlet.code.games;
 
-import hexlet.code.Engine;
 import hexlet.code.EngineNew;
 import hexlet.code.Utils;
 
 public class ProgressionNew {
 
-    private static String rule = "What number is missing in the progression?";
+    private static final String rule = "What number is missing in the progression?";
 
     public static void playProgression() {
         final int lengthArray = 10;
         String[] progressionLine = new String[lengthArray];
-        final int amountData = 2;
+        final int amountData = 6;
         String[] questionsAndAnswers = new String[amountData];
 
         final int rightBound = 21;
@@ -20,31 +19,29 @@ public class ProgressionNew {
         final int skipRightBound = 10;
 
 
-        for (int winStreak = 0; winStreak < Engine.getWinForEnd(); winStreak++) {
-            if (!Engine.itLose()) {
+        for (int i = 0; i < amountData; i += 2) {
+            int step = Utils.getRandomInt(stepLeftBound, stepRightBound);
+            int firstNum = Utils.getRandomInt(0, rightBound);
 
-                int step = Utils.getRandomInt(stepLeftBound, stepRightBound);
-                int firstNum = Utils.getRandomInt(0, rightBound);
-
-                progressionLine[0] = Integer.toString(firstNum);
-                for (int i = 1; i < progressionLine.length; i++) {
-                    progressionLine[i] = Integer.toString(Integer.parseInt(progressionLine[i - 1]) + step);
-                }
-
-                int skip = Utils.getRandomInt(0, skipRightBound);
-                String answer = progressionLine[skip];
-                progressionLine[skip] = "..";
-
-                String question = "\nQuestion: ";
-                for (int i = 0; i < progressionLine.length; i++) {
-                    question += progressionLine[i] + " ";
-                }
-
-                questionsAndAnswers[0] = question;
-                questionsAndAnswers[1] = answer;
-
-                EngineNew.run(rule, questionsAndAnswers);
+            progressionLine[0] = Integer.toString(firstNum);
+            for (int j = 1; j < progressionLine.length; j++) {
+                progressionLine[j] = Integer.toString(Integer.parseInt(progressionLine[j - 1]) + step);
             }
+
+            int skip = Utils.getRandomInt(0, skipRightBound);
+            String answer = progressionLine[skip];
+            progressionLine[skip] = "..";
+
+            String question = "\nQuestion: ";
+            for (int k = 0; k < progressionLine.length; k++) {
+                question += progressionLine[k] + " ";
+            }
+
+            questionsAndAnswers[i] = question;
+            questionsAndAnswers[i + 1] = answer;
         }
+
+        EngineNew.run(rule, questionsAndAnswers);
+
     }
 }
